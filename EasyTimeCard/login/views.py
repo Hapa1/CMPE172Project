40f2091d218be8b82c7a80e2956021f624b9a6ed
Shivi <shivi.2096@gmail.com>
2018-04-30 22:32:35 -0700

    changes

--- EasyTimeCard/login/views.py
+++ EasyTimeCard/login/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import json
-import django_saml2_auth.views
 
 # Create your views here.
 def index(request):
